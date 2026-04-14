81a2717306ef901f112a276c4b629f81de669f58
0xSt0le <stolepanaggio@gmail.com>
2026-04-14 19:14:20 +0200

    Fix wired DS4 PS/touchpad button masking in PS4 USB host parser

--- Firmware/RP2040/src/USBHost/HostDriver/PS4/PS4.cpp
+++ Firmware/RP2040/src/USBHost/HostDriver/PS4/PS4.cpp
@@ -18,7 +18,7 @@ void PS4Host::initialize(Gamepad& gamepad, uint8_t address, uint8_t instance, co
 void PS4Host::process_report(Gamepad& gamepad, uint8_t address, uint8_t instance, const uint8_t* report, uint16_t len)
 {
     std::memcpy(&in_report_, report, std::min(static_cast<size_t>(len), sizeof(PS4::InReport)));
-    in_report_.buttons[2] &= PS4::COUNTER_MASK;
+    in_report_.buttons[2] &= static_cast<uint8_t>(~PS4::COUNTER_MASK);
     if (std::memcmp(reinterpret_cast<const PS4::InReport*>(report), &prev_in_report_, sizeof(PS4::InReport)) == 0)
     {
         return;
